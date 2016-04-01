adc5ce6e757b1124520ebd4a09386d26ad53994e
Deborah Kurata <deborahk@insteptech.com>
2016-04-01 10:54:19 -0700

    Version that works with bound data.

--- MovieHunter/app/movies/movie-edit.component.ts
+++ MovieHunter/app/movies/movie-edit.component.ts
@@ -1,20 +1,17 @@
 import {Component, OnInit} from 'angular2/core';
-import { FORM_PROVIDERS, FormBuilder, ControlGroup, Control, FORM_DIRECTIVES, Validators } from 'angular2/common';
+import { FormBuilder, ControlGroup, Control, Validators } from 'angular2/common';
 import { ROUTER_DIRECTIVES, Router, RouteParams } from 'angular2/router';
 
 import { IMovie } from './movie';
 import { MovieService } from './movie.service';
 
 @Component({
-    templateUrl: 'app/movies/movie-edit.component.html',
-    directives: [ROUTER_DIRECTIVES, FORM_DIRECTIVES],
-    providers: [FORM_PROVIDERS]
+    templateUrl: 'app/movies/movie-edit.component.html'
 })
 export class MovieEditComponent implements OnInit {
     pageTitle: string = "Edit Movie";
     editForm: ControlGroup;
-    titleControl: Control;
-    directorControl: Control;
+    titleHasError: boolean=false;
     movie: IMovie;
     errorMessage: string;
 
@@ -22,30 +19,42 @@ export class MovieEditComponent implements OnInit {
         private _movieService: MovieService,
         private _router: Router,
         private _routeParams: RouteParams) {
-
-        this.titleControl = new Control('', Validators.compose([Validators.required, 
-                                                                Validators.minLength(3), 
-                                                                Validators.maxLength(50)]))
-        this.editForm = this._fb.group({
-            'title':this.titleControl
-        })
     }
 
     ngOnInit() {
         let id = +this._routeParams.get('id');
         this.getMovie(id);
-
     }
 
     getMovie(id: number) {
         this._movieService.getMovie(id)
             .subscribe(
-            movie => this.movie = movie,
+            movie => this.onMovieRetrieved(movie),
             error => this.errorMessage = <any>error);
     }
 
+    onMovieRetrieved(movie: IMovie) {
+        this.movie = movie;
+
+        this.editForm = this._fb.group({
+            'title':[this.movie.title,
+                      Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(50)])]
+        })
+        
+        this.editForm.valueChanges
+                .subscribe(data => this.onValueChanged(data));
+        // this.editForm.valueChanges
+        //         .debounceTime(500)
+        //         .subscribe(data => this.onValueChanged(data));
+    }
+
+    onValueChanged(data:any){
+        this.titleHasError = this.editForm.controls['title'].dirty && !this.editForm.controls['title'].valid;
+    }
+    
     saveMovie() {
         if (this.editForm.dirty && this.editForm.valid) {
             alert(`Title: ${this.editForm.value.title}`);
         }
     }
+}
