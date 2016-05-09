431cfbbbc2a04b2267ec224e2c2ca4ca1d1efaa0
johnlindquist <johnlindquist@gmail.com>
2016-05-09 17:24:05 -0600

    An example of moving validation into the templates and using .ng-invalid for styles.

--- MovieHunter/app/movies/movie-edit.component.ts
+++ MovieHunter/app/movies/movie-edit.component.ts
@@ -1,65 +1,40 @@
-import { Component } from '@angular/core';
-import { FormBuilder, ControlGroup, Control, Validators } from '@angular/common';
-import { ROUTER_DIRECTIVES, OnActivate, RouteSegment } from '@angular/router';
-
-import { IMovie } from './movie';
-import { MovieService } from './movie.service';
-import { NumberValidator } from '../shared/number.validator';
+import {Component} from '@angular/core';
+import {FormBuilder, ControlGroup, Control} from '@angular/common';
+import {ROUTER_DIRECTIVES, OnActivate, RouteSegment} from '@angular/router';
+import {IMovie} from './movie';
+import {MovieService} from './movie.service';
+import {RangeValidator} from '../shared/number.validator.directive';
 
 @Component({
     templateUrl: 'app/movies/movie-edit.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, RangeValidator]
 })
 export class MovieEditComponent implements OnActivate {
-    pageTitle: string = 'Edit Movie';
-    editForm: ControlGroup;
-    titleControl: Control;
-    formError: { [id: string]: string };
-    private _validationMessages: { [id: string]: { [id: string]: string } };
-    movie: IMovie;
-    errorMessage: string;
+    pageTitle:string = 'Edit Movie';
+    editForm:ControlGroup;
+    titleControl:Control;
+    movie:IMovie;
+    errorMessage:string;
 
-    constructor(private _fb: FormBuilder,
-        private _movieService: MovieService) {
+    errors;
 
-        // Initialization of strings
-        this.formError = {
-            'title': '',
-            'director': '',
-            'starRating': '',
-            'description': ''
-        };
-
-        this._validationMessages = {
-            'title': {
-                'required': 'Movie title is required',
-                'minlength': 'Movie title must be at least three characters.',
-                'maxlength': 'Movie title cannot exceed 50 characters.'
-            },
-            'director': {
-                'required': 'Director is required',
-                'minlength': 'Director must be at least 5 characters.',
-                'maxlength': 'Director cannot exceed 50 characters.'
-            },
-            'starRating': {
-                'range': 'Rate the movie between 1 (lowest) and 5 (highest).'
-            }
-        };
+    constructor(private _fb:FormBuilder,
+                private _movieService:MovieService) {
     }
 
-    routerOnActivate(curr: RouteSegment): void {
+    routerOnActivate(curr:RouteSegment):void {
         let id = +curr.getParam('id');
         this.getMovie(id);
     }
 
-    getMovie(id: number) {
+    getMovie(id:number) {
         this._movieService.getMovie(id)
             .subscribe(
-            movie => this.onMovieRetrieved(movie),
-            error => this.errorMessage = <any>error);
+                movie => this.onMovieRetrieved(movie),
+                error => this.errorMessage = <any>error);
     }
 
-    onMovieRetrieved(movie: IMovie) {
+    onMovieRetrieved(movie:IMovie) {
         this.movie = movie;
 
         if (this.movie.movieId === 0) {
@@ -68,48 +43,14 @@ export class MovieEditComponent implements OnActivate {
             this.pageTitle = `Edit Movie: ${this.movie.title}`;
         }
 
-        this.titleControl = new Control(this.movie.title, Validators.compose([Validators.required,
-                                                                Validators.minLength(3),
-                                                                Validators.maxLength(50)]));
+        this.titleControl = new Control(this.movie.title);
+
         this.editForm = this._fb.group({
             'title': this.titleControl,
-            'director': [this.movie.director,
-                Validators.compose([Validators.required,
-                    Validators.minLength(5),
-                    Validators.maxLength(50)])],
-            'starRating': [this.movie.starRating,
-                NumberValidator.range(1, 5)],
+            'director': [this.movie.director],
+            'starRating': [this.movie.starRating],
             'description': [this.movie.description]
         });
-
-        this.editForm.valueChanges
-            .map(value => {
-                // Causes infinite loop
-                // this.titleControl.updateValue(value.title.toUpperCase());
-                value.title = value.title.toUpperCase();
-                return value;
-            })
-            .subscribe(data => this.onValueChanged(data));
-        // this.editForm.valueChanges
-        //         .debounceTime(500)
-        //         .subscribe(data => this.onValueChanged(data));
-    }
-
-    onValueChanged(data: any) {
-        for (let field in this.formError) {
-            if (this.formError.hasOwnProperty(field)) {
-                let hasError = this.editForm.controls[field].dirty &&
-                    !this.editForm.controls[field].valid;
-                this.formError[field] = '';
-                if (hasError) {
-                    for (let key in this.editForm.controls[field].errors) {
-                        if (this.editForm.controls[field].errors.hasOwnProperty(key)) {
-                            this.formError[field] += this._validationMessages[field][key] + ' ';
-                        }
-                    }
-                }
-            }
-        }
     }
 
     saveMovie() {
--- MovieHunter/app/shared/number.validator.ts
+++ MovieHunter/app/shared/number.validator.ts
@@ -1,30 +1,24 @@
 import { Control } from '@angular/common';
 
-// For simple validation with no parameters.
-// Return type is a key/value pair
-interface IValidationResult {
-    [key: string]: boolean;
-}
-
-// For validation with parameters.
-// Return type is a function that takes in a control
-// and returns a key/value pair
-interface IValidationFunction {
-    (c: Control): IValidationResult;
-}
 
 export class NumberValidator {
 
-    static range(min: number, max: number): IValidationFunction {
+    static range(min, max){
         return (control: Control): { [key: string]: boolean } => {
-            if (control.value && (isNaN(control.value) || control.value < min || control.value > max)) {
-                return { 'range': true };
+            if (control.value < min ) {
+                return { 'min': true };
+            }
+            else if(control.value > max){
+                return { 'max' : true }
+            }
+            else if(isNaN(control.value)){
+                return { 'NaN': true }
             }
             return null;
         };
     }
 
-    static rangeHardCoded(control: Control): IValidationResult {
+    static rangeHardCoded(control: Control){
         if (control.value && (isNaN(control.value) || control.value < 1 || control.value > 5)) {
             return { 'range': true };
         }
